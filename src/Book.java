663a6eb1271f830bb78173ea41382af8018934ab
kila1 <kila14081996@gmail.com>
2024-03-23 14:28:33 +0300

    Домашняя работа №12. Работа над ошибками.

--- src/Book.java
+++ src/Book.java
@@ -2,10 +2,12 @@ public class Book {
 
     private final String name;
     private int yearPublishing;
+    private Author author;
 
-    public Book(String name, int yearPublishing) {
+    public Book(String name, int yearPublishing, Author author) {
         this.name = name;
         this.yearPublishing = yearPublishing;
+        this.author = author;
     }
 
     public String getName() {
@@ -18,5 +20,9 @@ public class Book {
 
     public void setYearPublishing(int yearPublishing) {
         this.yearPublishing = yearPublishing;
+    }
+
+    public Author getAuthor() {
+        return author;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        Book book = new Book("Тень и кость", 2021);
         Author book1 = new Author("Ли", "Бардуго");
+        Book book = new Book("Тень и кость", 2021, book1);
         book.setYearPublishing(2000);
         System.out.println("Книга под названием " + book.getName() + " "
-                + book.getYearPublishing() + " года издания");
-        System.out.println("Автор - " + book1.getFirstName() + " " + book1.getSurname() + ".");
+                + book.getYearPublishing() + " года издания. Автор - " + book.getAuthor().getFirstName()
+                + " " + book.getAuthor().getSurname());
 
-        Book book2 = new Book("Война и мир", 1869);
         Author book3 = new Author("Лев", "Толстой");
+        Book book2 = new Book("Война и мир", 1869, book3);
         System.out.println("Книга под названием " + book2.getName() + " "
-                + book2.getYearPublishing() + " года издания");
-        System.out.println("Автор - " + book3.getFirstName() + " " + book3.getSurname() + ".");
+                + book2.getYearPublishing() + " года издания. Автор - " + book2.getAuthor().getFirstName()
+                + " " + book2.getAuthor().getSurname());
     }
 }
